834d84dfa9d9cc0b6fbace63517e1dde7b79d80f
Emerson pinho <the.emerson.araujo@gmail.com>
2024-10-11 09:04:06 -0300

    commit inicial com algumas anotações para o primeiro passo.

---
+++ hashzap.py
@@ -0,0 +1,17 @@
+# Tela inicial:
+    # Titulo: hashzap
+    # Botao: iniciar chat
+        # Quando clicar no botao:
+        # Abrir um poup/modal/alerta
+            # Titulo: bem vindo ao Hashzap
+            # Caixa de texto: escreva seu nome no chat
+            # Botão: entrar no chat
+                # Quando clicar no botão
+                # Sumir com o titulo
+                # Sumir com botão inciar chat
+                    # Carregar chat
+                    # Carregar o caompo de enviar mensegem: "Digite sua mensagem"
+                    # Botão enviar
+                        # Enviar mensegem
+                        # Limpar a caixa de mensagem
+                        
